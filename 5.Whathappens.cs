35d1570d1360149ff9028f343b5d9402e5b73be5
Kshitiz Basnet <85289617+KshitizBasnet2021@users.noreply.github.com>
2025-01-06 23:26:08 -0600

    5. What happens

---
+++ 5.Whathappens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+/*
+What will happen if this code executes?"
+*/
+internal class WhatHappens
+{
+
+    public static void Main(string[] args)
+    {
+        int max = 500;
+        for (byte i = 0; i < max; i++)
+        {
+            Console.WriteLine(i);//added cuz missing
+        }
+
+
+    }
+}
+/* 
+ * The Answer is that the loop will go on and on forever
+ */
